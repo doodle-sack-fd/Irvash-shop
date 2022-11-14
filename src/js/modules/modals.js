681d1af67183d5e7d770fb4dd4d2b43d971ec789
Maxim <titov3000@mail.ru>
2022-11-14 10:49:10 +0300

    WindowScroll/BasicAnimation

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,6 +1,7 @@
 function closeModal(modal) {
     modal.style.display = 'none';
     document.body.style.overflow = '';
+    document.body.style.marginRight = `0px`;
 }
 
 const modals = () => {
@@ -8,13 +9,15 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
 
 
 
         function openModal(modal) {
             modal.style.display = display;
             document.body.style.overflow = 'hidden';
+            document.body.style.marginRight = `${scroll}px`;
         }
 
         trigger.forEach(elem => {
@@ -63,12 +66,29 @@ const modals = () => {
         }, time);
     }
 
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        /* Узнаем полную ширину блока DIV в том числе border,
+        отнимаем значение клиентской ширины ( padding/content) (!прокрутка не включается)*/
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    };
+
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
     bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
-    // showModalByTime('.popup', 60000);
+    showModalByTime('.popup', 60000);
 
 };
 
